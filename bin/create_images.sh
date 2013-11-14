30d9de65b041bd54c47810979e845cf7db73acaa
Bernd Oeggl <bernd@komplett.cc>
2013-11-14 09:40:10 +0100

    add SCRIPTDIR to execute script from anywhere

--- bin/create_images.sh
+++ bin/create_images.sh
@@ -5,8 +5,11 @@
 # if you want to log message use ... > log.txt 2>&1
 #
 
+SCRIPTDIR=$(readlink -f $(dirname $0))
+echo $SCRIPTDIR
+
 if [ -z "$1" ]; then 
-  SUBDIR="../songs/"
+  SUBDIR="$SCRIPTDIR/../songs/"
 else
   SUBDIR=$1
 fi
@@ -16,9 +19,9 @@ find $SUBDIR -name "*.chopro" -exec sh ./2png.sh {} \;
 
 # rebuild Songs.js
 echo "recreating ../lib/Songs.js ..."
-cd ../
+cd $SCRIPTDIR/../
 echo "window.SONGS = [" > ./lib/Songs.js
 find ./songs/ -name "*.png" -printf "\047%p\047\054\n" >> ./lib/Songs.js
 echo "'' // last empty record" >> ./lib/Songs.js
 echo "]" >> ./lib/Songs.js
-cd ./bin/
+cd $SCRIPTDIR
